1fac2aa6b71bc6afc7b0fd38cc7e6e2d358fa237
AlessioMartella7 <alessio.mart87@gmail.com>
2024-09-20 18:22:54 +0200

    added method for ajax call to get searched movieName

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,7 @@
 <script>
 import AppHeaderSearch from './AppHeaderSearch.vue';
+import axios from 'axios';
+import {store} from '../store.js'
 
 
 export default {
@@ -8,12 +10,29 @@ export default {
   },
   data() {
     return {
+      store,
+      apiSearchMovie:'https://api.themoviedb.org/3/search/movie',
+      apiKey: '4377ddd6b96700ad11f5d9d8592df891',
       
     }
   },
   methods:{
-     searchedResult (message){
-      console.log('AppHeader ha ricevuto il messaggio', message)
+     getSearchedMovie(movieName){
+        axios.get(this.apiSearchMovie, {
+          params: {
+            api_key: this.apiKey,
+            query: movieName,
+          }
+        })
+        .then((response) => {
+          store.movieList = response.data.results;
+          console.log('RISULTATO CHIAMATA:', store.movieList)
+        })
+     } ,
+
+     searchedResult (searchedMovieName){
+      console.log('AppHeader ha ricevuto il messaggio', searchedMovieName)
+      this.getSearchedMovie(searchedMovieName)
      },
   },
 }
