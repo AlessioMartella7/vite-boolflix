600b129933c21457db9889441e6c465d061c3e17
AlessioMartella7 <alessio.mart87@gmail.com>
2024-09-23 16:11:12 +0200

    added list of languages in store.jg

--- src/components/AppMainMoviesItem.vue
+++ src/components/AppMainMoviesItem.vue
@@ -6,6 +6,12 @@ export default {
   data() {
     return {
       store,
+
+    }
+  },
+  methods:{
+    getFlagImg(iso){
+      
     }
   }
 }
--- src/store.js
+++ src/store.js
@@ -3,4 +3,13 @@ import {reactive} from "vue";
 export const store = reactive ({
     mediaMovieList: [],
     mediaSeriesList: [],
+    languageToCountry: {
+        en: 'gb',
+        fr: 'fr',
+        de: 'de',
+        it: 'it',
+        es: 'es',
+        zh: 'cn',
+        ja: 'jp',
+      }
 });
